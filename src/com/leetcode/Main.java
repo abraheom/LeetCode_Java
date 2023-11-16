d9a956094e234ef11bf599fad14ec752b72b64a2
Abraham Garcia <abraheom@gmail.com>
2023-11-16 12:43:08 -0600

    Se modifico clase Main

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -25,17 +25,12 @@ public class Main {
 		pm.agregarProblema(new P2().setInfo(2, "Add Two Numbers"));
 		pm.agregarProblema(new P3().setInfo(3, "Longest Substring Without Repeating Characters"));
 		pm.agregarProblema(new P4().setInfo(4, "Median of Two Sorted Arrays"));
-<<<<<<< HEAD
 		pm.agregarProblema(new P5().setInfo(5, "Longest Palindromic Substring"));
 		pm.agregarProblema(new P6().setInfo(6, "Zigzag Conversion")); //TODO Terminar
 		pm.agregarProblema(new P8().setInfo(8, "String to Integer (atoi)"));
 		pm.agregarProblema(new P6().setInfo(6, "Zigzag Conversion"));
 		pm.agregarProblema(new P1759().setInfo(1759, "Count Number of Homogenous Substrings")); // TODO Optimizar
 		pm.agregarProblema(new P214().setInfo(214, "Shortest Palindrome")); // TODO Optimizar
-=======
-		pm.agregarProblema(new P6().setInfo(6, "Zigzag Conversion")); //TODO Terminar
-		pm.agregarProblema(new P8().setInfo(8, "String to Integer (atoi)"));
->>>>>>> branch 'main' of git@github.com:abraheom/LeetCode_Java.git
 
 		// Ejecutar problemas
 		pm.ejecutarUltimoProblema();
